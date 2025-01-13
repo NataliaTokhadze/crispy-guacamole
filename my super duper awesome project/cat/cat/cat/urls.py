86aa80fd8fa5fad936b31f350f36a0843175c35d
Natalia <natalia.tokhadze@gmail.com>
2025-01-13 14:55:46 +0400

    finish 2 video

--- my super duper awesome project/cat/cat/cat/urls.py
+++ my super duper awesome project/cat/cat/cat/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', meow_views.index, name='index'),
+    path('', meow_views.index.as_view(), name='index'),
     path('meow/', include('meow.urls')),
     path('accounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- my super duper awesome project/cat/cat/meow/views.py
+++ my super duper awesome project/cat/cat/meow/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Video
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-def index(request):
-    return render(request, 'meow/index.html')
+class index(ListView):
+    model = Video
+    template_name = 'meow/index.html'
+    order_by = '-date_posted'
 
 class CreateVideo(LoginRequiredMixin, CreateView):
     model = Video
