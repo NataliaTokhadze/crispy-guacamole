c1a9ab2aafe4d6cfb4b8b88fb36e01d78e066b5e
Natalia <natalia.tokhadze@gmail.com>
2025-01-17 16:55:58 +0400

    problem category list

--- my super duper awesome project/cat/cat/meow/views.py
+++ my super duper awesome project/cat/cat/meow/views.py
@@ -3,7 +3,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views import View
-from .models import Video, Comment
+from .models import Video, Comment, Category
 from .forms import CommentForm
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -31,9 +31,12 @@ class DetailVideo(View):
 
         form = CommentForm()
         comments = Comment.objects.filter(video=video).order_by('-created_on')
+        categories = Video.objects.filter(category=video.category)[:15]
+
         context = {
             'object' : video,
             'comments' : comments,
+            'categories' : categories,
             'form': form
         }
         return render(request, 'meow/detail_video.html', context)
@@ -51,9 +54,13 @@ class DetailVideo(View):
             comment.save()
 
         comments = Comment.objects.filter(video=video).order_by('-created_on')
+        categories = Video.objects.filter(category=video.category)[:15]
+
+
         context = {
             'object': video,
             'comments': comments,
+            'categories' : categories,
             'form': form
         }
         return render(request, 'meow/detail_video.html', context)
@@ -80,3 +87,14 @@ class DeleteVideo(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         video = self.get_object()
         return self.request.user == video.uploader
+    
+class VideoCategoryList(View):
+    def get(self, request, pk, *args, **kwargs):
+        category = Category.objects.get(pk=pk)
+        videos = Video.objects.filter(category=pk).order_by('-date_posted')
+        context = {
+            'category': category,
+            'videos': videos
+        }
+
+        return render(request, 'meow/video_category.html', context)
